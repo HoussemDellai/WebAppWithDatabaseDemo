c49cfba0122276ee0283695f9f8228a7e4719136
Houssem Dellai <houssem.dellai@live.com>
2018-07-18 23:55:15 +0200

    Added new Address column

--- WebApp.Database/dbo/Tables/Employee.sql
+++ WebApp.Database/dbo/Tables/Employee.sql
@@ -4,6 +4,7 @@
     [Fullname]   NVARCHAR (MAX) NULL,
     [Email]		 NVARCHAR (50)  NULL, 
 	[Phone]		 NVARCHAR (50)  NULL, 
+	[Address]	 NVARCHAR (50)  NULL, 
     CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
--- WebApp/Models/Employee.cs
+++ WebApp/Models/Employee.cs
@@ -7,5 +7,6 @@
         public string Department { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public string Address { get; set; }
     }
 }
