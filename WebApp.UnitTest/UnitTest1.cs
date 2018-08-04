48ceb46f84a7d9755cbe7eb45499393568b6b060
Houssem Dellai <houssem.dellai@live.com>
2018-08-04 18:13:29 +0200

    Added unit test project

---
+++ WebApp.UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApp.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UnitTestShouldAlwaysWorkTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void OnePlusTwoEqualThreeTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
