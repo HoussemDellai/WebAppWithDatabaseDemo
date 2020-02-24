90d0a0e7ca5b3ae4f412affdf720095013d527ed
Bairagi Charan Rout <47815569+routba1@users.noreply.github.com>
2020-02-25 02:23:49 +0530

    Update EmployeesUiTests.cs

--- SeleniumUiTests/EmployeesUiTests.cs
+++ SeleniumUiTests/EmployeesUiTests.cs
@@ -15,7 +15,7 @@ namespace SeleniumUiTests
     [TestClass]
     public class EmployeesUiTests
     {
-        private string _websiteURL = "https://ignite-webapp-test-997.azurewebsites.net/";
+        private string _websiteURL = "https://azuredevopspoc-dev-webapp.azurewebsites.net/";
         private RemoteWebDriver _browserDriver;
         public TestContext TestContext { get; set; }
 
