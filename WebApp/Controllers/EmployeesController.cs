e78388f4e799d24a344695cd949fad7ca72162ef
DhruvAgrawal16 <42446527+DhruvAgrawal16@users.noreply.github.com>
2020-06-17 23:35:47 +0530

    Update EmployeesController.cs

--- WebApp/Controllers/EmployeesController.cs
+++ WebApp/Controllers/EmployeesController.cs
@@ -27,7 +27,7 @@ namespace WebApp.Controllers
         // GET: Employees/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
+            if (id == null && id!="dhruv")
             {
                 return NotFound();
             }
