26b1f7ae7d782441ee768fa92cd81eb59f9d225c
Houssem Dellai <houssem.dellai@live.com>
2020-06-23 07:52:54 +0200

    Update webapp-db.tf

--- Terraform/webapp-db.tf
+++ Terraform/webapp-db.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource-group-name}"
-  location = "${var.location}"
+  name     = var.resource-group-name
+  location = var.location
 }
 
 resource "azurerm_app_service_plan" "plan" {
   name                = "example-appserviceplan"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   sku {
     tier = "Standard"
@@ -15,10 +15,10 @@ resource "azurerm_app_service_plan" "plan" {
 }
 
 resource "azurerm_app_service" "app-service" {
-  name                = "${var.app-service-name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.plan.id}"
+  name                = var.app-service-name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -38,8 +38,8 @@ resource "azurerm_app_service" "app-service" {
 
 resource "azurerm_sql_server" "sqldb" {
   name                         = "terraform-sqlserver"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
-  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "houssem"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
@@ -47,9 +47,9 @@ resource "azurerm_sql_server" "sqldb" {
 
 resource "azurerm_sql_database" "db" {
   name                = "terraform-sqldatabase"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  server_name         = "${azurerm_sql_server.sqldb.name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sqldb.name
 
   tags = {
     environment = "production"
